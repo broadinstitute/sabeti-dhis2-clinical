5fb96ff4a41a4f3bde3c10dfbef9d439e7110a73
jotasolano <jsolano91@gmail.com>
2017-07-17 17:03:21 -0400

    fix(d3 geojson) rendering and updating working

--- src/index.js
+++ src/index.js
@@ -35,7 +35,23 @@ let getData = DataLoader()
     // console.log(casesByDate);
     d3.select('#timeline').datum(casesByDate).call(timeline);
 
-    let map = L.map('map').setView([8.4506145, -11.3474766], 8);
+    //an array with only objects that have a valid lat and long
+    let locationArr = allCases.filter(function(el) { return !(el.lat === undefined || el.lng === undefined) })
+
+    let geoArr = geoCases.filter(function(el, i){
+      return !(el.features[0].geometry.coordinates[0] === undefined || el.features[0].geometry.coordinates[1] === undefined)
+    })
+
+    let map;
+    main(geoArr)
+
+    function main(data){
+      addLmaps();
+      drawFeatures(data);   
+    }
+
+    function addLmaps() {
+        map = L.map('map').setView([8.4506145, -11.3474766], 8);
         let mapLink = 
           '<a href="http://openstreetmap.org">OpenStreetMap</a>';
         L.tileLayer(
@@ -44,13 +60,9 @@ let getData = DataLoader()
           maxZoom: 18,
           }).addTo(map);
 
-    //an array with only objects that have a valid lat and long
-    let locationArr = allCases.filter(function(el) { return !(el.lat === undefined || el.lng === undefined) })
+        L.svg().addTo(map);
 
-    let geoArr = geoCases.filter(function(el, i){
-      return !(el.features[0].geometry.coordinates[0] === undefined || el.features[0].geometry.coordinates[1] === undefined)
-    })
-
+        
     locationArr.forEach(d => { marker = new L.marker([d.lat, d.lng]).addTo(map); });
 
     locationArr.forEach(function(d) {
@@ -58,55 +70,34 @@ let getData = DataLoader()
     })
 
 
-    //initialize svg layer
-
-    let svgLayer = L.svg();
-    svgLayer.addTo(map);
-    let projectPoint;
-
-    let svg = d3.select("#map").select("svg");
-    let g = d3.select("#map").select("svg").select('g');
-    g.attr("class", "leaflet-zoom-hide");
-
-    let transform = d3.geoTransform({point: projectPoint}),
-        path = d3.geoPath().projection(transform);
-
-    let d3_features = g.selectAll('path')
-      .datum({type: "FeatureCollection", features: geoArr.features})
-      .enter().append('path');
-
-    map.on('viewreset', reset);
-
-    reset();
-
-    function reset() {
-        
-    let bounds = map.getBounds(),
-        topLeft = map.latLngToLayerPoint(bounds.getNorthWest()),
-        bottomRight = map.latLngToLayerPoint(bounds.getSouthEast()),
-        existing = d3.set(),
-        drawLimit = bounds.pad(0.4);
-
-      svg.style("width", map.getSize().x + 'px')
-      .style("height", map.getSize().y + 'px')
-      .style("margin-left", topLeft.x + "px")
-      .style("margin-top", topLeft.y + "px");
-
-      g.attr("transform", "translate(" + (-topLeft.x) + "," + (-topLeft.y) + ")");
-
-      // initialize the path data 
-      d3_features.attr("d", path)
-        .style("fill-opacity", 0.7)
-        .attr('fill','blue');
-
-
-    // Use Leaflet to implement a D3 geometric transformation.
-      function projectPoint(x, y) {
-        let point = map.latLngToLayerPoint(new L.LatLng(y, x));
-        this.stream.point(point.x, point.y);
-      }
     }
 
+    function projectPoint(x, y) {
+      let point = map.latLngToLayerPoint(new L.LatLng(y, x));
+      this.stream.point(point.x, point.y);
+    }
+
+    function drawFeatures(data) {
+        var svg = d3.select("#map").select("svg");
+        
+        var transform = d3.geoTransform({point: projectPoint});
+        var path = d3.geoPath().projection(transform);
+    
+        var featureElement = svg.selectAll("path")
+            .data(data)
+            .enter()
+            .append("path")
+            .attr("stroke", "gray")
+            .attr("fill", "red")
+            .attr("fill-opacity", 1);
+    
+        map.on("moveend", update);
+        update();
+    
+        function update() {   
+            featureElement.attr("d", path);
+        } 
+    }
   }); //-->END .on('loaded')
 
 
